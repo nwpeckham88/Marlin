28635c8541ee97c61ae10bff045fa2cc2e3ae3f7
nwpeckham88 <74386527+nwpeckham88@users.noreply.github.com>
2021-09-04 21:04:04 -0500

    Changed display.
    Changed from FULL_GRAPHIC REPRAP controller to just DISCOUNT_SMART_CONTROLLER

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2188,7 +2188,7 @@
 //
 // Note: Usually sold with a white PCB.
 //
-//#define REPRAP_DISCOUNT_SMART_CONTROLLER
+#define REPRAP_DISCOUNT_SMART_CONTROLLER
 
 //
 // GT2560 (YHCB2004) LCD Display
@@ -2346,7 +2346,7 @@
 // RepRapDiscount FULL GRAPHIC Smart Controller
 // https://reprap.org/wiki/RepRapDiscount_Full_Graphic_Smart_Controller
 //
-#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
+//#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
 
 //
 // K.3D Full Graphic Smart Controller
