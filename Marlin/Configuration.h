cde9031283fbf9bb5b7c0187d8eb40b21491e31f
nwpeckham88 <74386527+nwpeckham88@users.noreply.github.com>
2021-10-08 07:32:26 -0500

    Made it more clear this is BEnder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -69,7 +69,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(KN8, Creality 4.2.7 w/ BLT)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(KN8, BENDER Creality 4.2.7 w/ BLT)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
